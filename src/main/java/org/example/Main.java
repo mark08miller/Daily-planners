7d65ebb3f6d82ea61cbe59f0bea90cd7a727558f
Mark MIller <youtte1g@gmail.com>
2022-11-06 21:14:41 +0200

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] result = {"Джо", "Роберт", "Макс","Стинг"};
+        String finalRes = createDailyPlan(result);
+        System.out.println(finalRes);
+    }
+    public static String createDailyPlan(String[] plans) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < plans.length; i++) {
+            builder.append(i + 1).append(".").append(")").append(plans[i]);
+        }
+        return String.valueOf(builder);
     }
 }
