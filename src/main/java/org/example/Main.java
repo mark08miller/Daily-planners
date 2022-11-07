e83ab715602181675480821abc827e73df916442
Mark MIller <youtte1g@gmail.com>
2022-11-07 19:40:15 +0200

    Final commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String[] result = {"Джо", "Роберт", "Макс","Стинг"};
+        String[] result = {};
         String finalRes = createDailyPlan(result);
         System.out.println(finalRes);
     }
+
     public static String createDailyPlan(String[] plans) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < plans.length; i++) {
-            builder.append(i + 1).append(".").append(")").append(" ").append(plans[i]).append(";").append("\n");
+            builder.append("\n")
+                    .append(i + 1)
+                    .append(".")
+                    .append(") ")
+                    .append(plans[i]).append(";");
         }
         String name = String.valueOf(builder);
-        return "My plans:" + "\n"  + name;
+        return "My plans:" + name;
     }
 }
